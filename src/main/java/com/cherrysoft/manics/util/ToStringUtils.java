826ddd495487457b94779c694b0fb2a10c8d9802
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-16 11:31:51 -0600

    Added ApplicationTimeUtils and ToStringUtils

---
+++ src/main/java/com/cherrysoft/manics/util/ToStringUtils.java
@@ -0,0 +1,32 @@
+package com.cherrysoft.manics.util;
+
+import com.cherrysoft.manics.exception.v2.ApplicationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ToStringUtils {
+  private static final ObjectMapper MAPPER = new ObjectMapper().findAndRegisterModules();
+
+  private static final DefaultPrettyPrinter PRETTY_PRINTER = new DefaultPrettyPrinter()
+      .withObjectIndenter(new DefaultIndenter().withIndent("  "))
+      .withArrayIndenter(new DefaultIndenter().withIndent("  "));
+
+  public static String toJsonString(final Object object) {
+    return toJsonString(object, JsonStyle.DEFAULT);
+  }
+
+  public static String toJsonString(final Object object, final JsonStyle jsonStyle) {
+    try {
+      if (jsonStyle == JsonStyle.PRETTY) {
+        return MAPPER.writer(PRETTY_PRINTER).writeValueAsString(object);
+      } else {
+        return MAPPER.writeValueAsString(object);
+      }
+    } catch (JsonProcessingException e) {
+      throw new ApplicationException(e);
+    }
+  }
+
+}
