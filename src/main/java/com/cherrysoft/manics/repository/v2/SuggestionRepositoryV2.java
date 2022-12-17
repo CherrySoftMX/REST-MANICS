7cd0835c23e2331ee477abadf6669f05cc72590d
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-16 18:25:42 -0600

    Added SuggestionService and SuggestionRepository classes

---
+++ src/main/java/com/cherrysoft/manics/repository/v2/SuggestionRepositoryV2.java
@@ -0,0 +1,16 @@
+package com.cherrysoft.manics.repository.v2;
+
+import com.cherrysoft.manics.model.v2.SuggestionV2;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SuggestionRepositoryV2 extends JpaRepository<SuggestionV2, Long> {
+
+  @Query("FROM SuggestionV2 s JOIN s.user user WHERE user.id = :userId")
+  List<SuggestionV2> findAllSuggestionsOfUser(Long userId);
+
+}
