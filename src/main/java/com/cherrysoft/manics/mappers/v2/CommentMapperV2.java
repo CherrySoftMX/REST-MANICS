ad8ed419a7158f75709822257015c7009ec3464b
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-17 17:31:43 -0600

    Added CommentDTO and CommentMapper classes

---
+++ src/main/java/com/cherrysoft/manics/mappers/v2/CommentMapperV2.java
@@ -0,0 +1,18 @@
+package com.cherrysoft.manics.mappers.v2;
+
+import com.cherrysoft.manics.controller.v2.dto.CommentDTO;
+import com.cherrysoft.manics.model.v2.CommentV2;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapperV2 {
+
+  CommentDTO toDto(CommentV2 comment);
+
+  List<CommentDTO> toDtoList(List<CommentV2> comments);
+
+  CommentV2 toComment(CommentDTO commentDto);
+
+}
