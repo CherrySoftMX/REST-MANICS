504547a60a3d4a188faa2ad7cd655ea429d332c2
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-17 11:37:11 -0600

    Added CartoonDTO, CartoonResponseDTO, ChapterDTO, PageDTO and CartoonMapper classes

---
+++ src/main/java/com/cherrysoft/manics/controller/v2/dto/cartoons/ChapterDTO.java
@@ -0,0 +1,26 @@
+package com.cherrysoft.manics.controller.v2.dto.cartoons;
+
+import com.cherrysoft.manics.model.v2.ChapterV2;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class ChapterDTO {
+  @NotBlank(message = "The name of the chapter MUST be provided.")
+  private final String name;
+
+  @NotNull(message = "The chapter number for this chapter MUST be provided.")
+  private final Integer chapterNumber;
+
+  @NotNull(message = "The publication date of the chapter MUST be provided with the format: " + ChapterV2.MONTH_DAY_YEAR_PATTERN)
+  private final LocalDate publicationDate;
+
+  @NotNull(message = "The total pages of the chapter MUST be provided.")
+  private final Integer totalPages;
+
+  private final List<PageDTO> pages;
+}
