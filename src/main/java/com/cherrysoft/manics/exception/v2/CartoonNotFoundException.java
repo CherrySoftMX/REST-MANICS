d91c44f7e73d2315976fe359e05525c3e0f24ef9
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-18 10:39:30 -0600

    Added BookmarkRepository and BookmarkService classes

--- src/main/java/com/cherrysoft/manics/exception/v2/CartoonNotFoundException.java
+++ src/main/java/com/cherrysoft/manics/exception/v2/CartoonNotFoundException.java
@@ -8,4 +8,8 @@ public class CartoonNotFoundException extends RuntimeException {
     super(String.format("%s with id: %s not found.", cartoonType.name(), id));
   }
 
+  public CartoonNotFoundException(Long id) {
+    super(String.format("Cartoon with id: %s not found.", id));
+  }
+
 }
