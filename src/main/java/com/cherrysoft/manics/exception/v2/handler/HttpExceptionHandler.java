a1b527fd299d91f82d144238d5d1f44aa39ff11d
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-21 17:27:09 -0600

    Added logging when a validation error occurs

--- src/main/java/com/cherrysoft/manics/exception/v2/handler/HttpExceptionHandler.java
+++ src/main/java/com/cherrysoft/manics/exception/v2/handler/HttpExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.cherrysoft.manics.exception.v2.handler;
 
 import com.cherrysoft.manics.exception.v2.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
@@ -10,12 +11,14 @@ import org.springframework.lang.NonNull;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
@@ -28,11 +31,30 @@ public class HttpExceptionHandler extends ResponseEntityExceptionHandler {
       HttpStatus status,
       WebRequest request
   ) {
-    final Map<String, String> validationErrors = new LinkedHashMap<>();
+    Map<String, String> validationErrors = extractValidationErrors(ex);
+    String uri = extractUri(request);
+    String verb = extractHttpVerb(request);
+    ErrorResponse errorResponse = new ErrorResponse(status, validationErrors);
+    log.warn("Verb: {} | URI: {} | Validation errors: {}", verb, uri, errorResponse.getValidationErrors());
+    return new ResponseEntity<>(errorResponse, status);
+  }
+
+  private Map<String, String> extractValidationErrors(MethodArgumentNotValidException ex) {
+    Map<String, String> result = new LinkedHashMap<>();
     ex.getBindingResult().getAllErrors().forEach(
-        e -> validationErrors.put(((FieldError) e).getField(), e.getDefaultMessage())
+        e -> result.put(((FieldError) e).getField(), e.getDefaultMessage())
     );
-    return new ResponseEntity<>(new ErrorResponse(status, validationErrors), status);
+    return result;
+  }
+
+  private String extractUri(WebRequest webRequest) {
+    ServletWebRequest servletWebRequest = (ServletWebRequest) webRequest;
+    return servletWebRequest.getRequest().getRequestURI();
+  }
+
+  private String extractHttpVerb(WebRequest webRequest) {
+    ServletWebRequest servletWebRequest = (ServletWebRequest) webRequest;
+    return servletWebRequest.getHttpMethod().name();
   }
 
 }
