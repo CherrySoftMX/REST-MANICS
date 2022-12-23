c60baa5d97d38afcb52442b4e25ff5c2e76a9577
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-23 12:35:43 -0600

    Added elasticsearch client config

--- src/main/java/com/cherrysoft/manics/config/ElasticSearchClientConfig.java
+++ src/main/java/com/cherrysoft/manics/config/ElasticSearchClientConfig.java
@@ -1,17 +1,13 @@
-package com.cherrysoft.manics.config.lagacy;
+package com.cherrysoft.manics.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "io.pratik.elasticsearch.repositories")
-@ComponentScan(basePackages = {"io.pratik.elasticsearch"})
 public class ElasticSearchClientConfig extends AbstractElasticsearchConfiguration {
 
   @Bean
