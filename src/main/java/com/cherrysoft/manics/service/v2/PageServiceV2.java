6c05c6f461a29b4337fd3624469f3333dce26f4f
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-23 12:41:03 -0600

    Added functionality to index, update and delete pages in elasticsearch

--- src/main/java/com/cherrysoft/manics/service/v2/PageServiceV2.java
+++ src/main/java/com/cherrysoft/manics/service/v2/PageServiceV2.java
@@ -4,17 +4,22 @@ import com.cherrysoft.manics.exception.v2.PageNotFoundException;
 import com.cherrysoft.manics.model.v2.ChapterV2;
 import com.cherrysoft.manics.model.v2.PageV2;
 import com.cherrysoft.manics.repository.v2.PageRepositoryV2;
+import com.cherrysoft.manics.service.v2.search.SearchingPageService;
 import com.cherrysoft.manics.util.BeanUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static java.util.Objects.nonNull;
 
 @Service
 @RequiredArgsConstructor
 public class PageServiceV2 {
   private final ChapterServiceV2 chapterService;
+  private final SearchingPageService searchingPageService;
   private final PageRepositoryV2 pageRepository;
 
   public PageV2 getPageById(Long id) {
@@ -27,20 +32,30 @@ public class PageServiceV2 {
     return pageRepository.findPageV2sByChapter_Id(chapterId, pageable);
   }
 
+  @Transactional
   public PageV2 createPage(Long chapterId, PageV2 newPage) {
     ChapterV2 chapter = chapterService.getChapterReferenceById(chapterId);
     newPage.setChapter(chapter);
-    return pageRepository.save(newPage);
+    PageV2 result = pageRepository.save(newPage);
+    searchingPageService.indexPageForSearching(result);
+    return result;
   }
 
+  @Transactional
   public PageV2 updatePage(Long id, PageV2 updatedPage) {
     PageV2 page = getPageById(id);
     BeanUtils.copyProperties(updatedPage, page);
-    return pageRepository.save(page);
+    PageV2 result = pageRepository.save(page);
+    if (nonNull(updatedPage.getImageUrl())) {
+      searchingPageService.updateIndexedPage(result);
+    }
+    return result;
   }
 
+  @Transactional
   public PageV2 deletePage(Long id) {
     PageV2 page = getPageById(id);
+    searchingPageService.deleteIndexedPage(page);
     pageRepository.deleteById(id);
     return page;
   }
