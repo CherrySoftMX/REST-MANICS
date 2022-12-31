2478ade483f00fd8a22bb503ad0c7b1b2ff6219a
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-31 15:59:35 -0600

    Added CartoonController class

--- src/main/java/com/cherrysoft/manics/model/CartoonType.java
+++ src/main/java/com/cherrysoft/manics/model/CartoonType.java
@@ -1,6 +1,16 @@
 package com.cherrysoft.manics.model;
 
+import java.util.stream.Stream;
+
 public enum CartoonType {
   COMIC,
   MANGA;
+
+  public static CartoonType of(String type) {
+    return Stream.of(CartoonType.values())
+        .filter(t -> t.name().equalsIgnoreCase(type))
+        .findFirst()
+        .orElseThrow();
+  }
+
 }
