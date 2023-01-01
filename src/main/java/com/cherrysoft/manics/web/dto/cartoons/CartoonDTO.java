4be517a960c25f9067cffe1af7e123be3998b33c
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-12-31 23:30:30 -0600

    Added hal-forms support for cartoon entity

--- src/main/java/com/cherrysoft/manics/web/dto/cartoons/CartoonDTO.java
+++ src/main/java/com/cherrysoft/manics/web/dto/cartoons/CartoonDTO.java
@@ -1,12 +1,15 @@
 package com.cherrysoft.manics.web.dto.cartoons;
 
 import com.cherrysoft.manics.web.dto.chapters.ChapterDTO;
+import com.cherrysoft.manics.web.dto.validation.OnCreate;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.Year;
 import java.util.List;
 import java.util.Set;
 
@@ -14,8 +17,29 @@ import java.util.Set;
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CartoonDTO {
-  @JsonUnwrapped
-  private final BaseCartoonFields baseCartoonFields;
+  @NotBlank(
+      message = "The name of the cartoon MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final String name;
+
+  @NotBlank(
+      message = "The author of the cartoon MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final String author;
+
+  @NotNull(
+      message = "Publication year of the cartoon MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final Year publicationYear;
+
+  @NotNull(
+      message = "Available chapters count MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final Integer availableChapters;
 
   private final List<ChapterDTO> chapters;
 
--- src/main/java/com/cherrysoft/manics/web/dto/cartoons/CartoonResponseDTO.java
+++ src/main/java/com/cherrysoft/manics/web/dto/cartoons/CartoonResponseDTO.java
@@ -1,12 +1,16 @@
 package com.cherrysoft.manics.web.dto.cartoons;
 
 import com.cherrysoft.manics.model.CartoonType;
+import com.cherrysoft.manics.web.dto.validation.OnCreate;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.Year;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -18,6 +22,27 @@ public class CartoonResponseDTO extends RepresentationModel<CartoonResponseDTO> 
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private final CartoonType type;
 
-  @JsonUnwrapped
-  private final BaseCartoonFields baseCartoonFields;
+  @NotBlank(
+      message = "The name of the cartoon MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final String name;
+
+  @NotBlank(
+      message = "The author of the cartoon MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final String author;
+
+  @NotNull(
+      message = "Publication year of the cartoon MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final Year publicationYear;
+
+  @NotNull(
+      message = "Available chapters count MUST be provided.",
+      groups = OnCreate.class
+  )
+  private final Integer availableChapters;
 }
